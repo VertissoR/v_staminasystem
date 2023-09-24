18acd7b1f10bfb6cac72523811d55093e48c8eb0
VertissoR <142553047+VertissoR@users.noreply.github.com>
2023-09-24 04:13:36 +0200

    Update config.lua
    Config for the new update v1.0.2

--- config.lua
+++ config.lua
@@ -28,3 +28,13 @@ V_Stam.DrainStamOnJumpingMultiplier = 0.5 -- [default: 0.5] This seems optimal t
 
 V_Stam.DrainStamOnClimbing = true -- [default: true] If true, stamina will drain when climbing obstacles (also ladders)
 V_Stam.DrainStamOnClimbingMultiplier = 0.2 -- [default: 0.2]
+
+V_Stam.DrainStamOnMelee = true -- [default: true] If true, stamina will drain when melee attacking
+V_Stam.DrainStamOnMeleeMultiplier = 0.1 -- [default: 0.1]
+V_Stam.DisableMelee = true -- [default: true] if true, player will not be able to melee when stamina reaches the threshold
+V_Stam.DisableMeleeThreshold = 5 -- [default: 5]
+
+V_Stam.MeleeNotify = false -- [default: true] if true, will show a notification when player is too tired to fight and tries to
+V_Stam.NotifyType = "STANDALONE" -- [default: 'STANDALONE'] Options: 'STANDALONE', 'ESX', 'MYTHIC', "CUSTOM", to add own notification, use "CUSTOM" and add it on line 171 in the cl_stam.lua
+V_Stam.MeleeNotifyText = 'You are too tired to fight!' -- [default: 'You are too tired to fight!'] The text to show when player is too tired to fight,
+-- if you're using "STANDALONE" Notify system and will change the text, be ready to play with it's coordinates in cl_stam/cl_stam.lua (line 225) since it's not the default fivem notification. and it may not be centered
